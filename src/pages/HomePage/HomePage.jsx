4208b7d8eb07751ea3f1da1d97b12987706fd6a8
E-gap <106596181+E-gap@users.noreply.github.com>
2023-06-04 01:20:37 +0300

    update link

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,18 +1,18 @@
-import css from "./HomePage.module.css";
+import css from './HomePage.module.css';
 
-import picture from "../../images/picture-for-item.png";
-import user1 from "../../images/user1.jpg";
-import user2 from "../../images/user2.jpg";
-import user3 from "../../images/user3.jpg";
-import user4 from "../../images/user4.jpg";
-import user5 from "../../images/user5.jpg";
-import user6 from "../../images/user6.jpg";
+import picture from '../../images/picture-for-item.png';
+import user1 from '../../images/user1.jpg';
+import user2 from '../../images/user2.jpg';
+import user3 from '../../images/user3.jpg';
+import user4 from '../../images/user4.jpg';
+import user5 from '../../images/user5.jpg';
+import user6 from '../../images/user6.jpg';
 
 const HomePage = () => {
   return (
     <div className={css.homePage}>
       <div className={css.container}>
-        <a href="/tweets" className={css.moveToTweets}>
+        <a href="/tweets-pr/tweets" className={css.moveToTweets}>
           Go to Tweets
         </a>
         <img src={picture} alt="head" className={css.picture} />
