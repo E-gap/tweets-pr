ce6a0eae5f9d1329c13b354561d78ceb89f8c86e
E-gap <106596181+E-gap@users.noreply.github.com>
2023-06-04 01:30:44 +0300

    up

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import { Route, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage/HomePage";
-import TweetPage from "./pages/TweetPage/TweetPage";
-import { RestrictedRoute } from "../src/utils/RestrictedRoute";
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage/HomePage';
+import TweetPage from './pages/TweetPage/TweetPage';
+import { RestrictedRoute } from '../src/utils/RestrictedRoute';
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/tweets" element={<TweetPage />} />
+        <Route path="/tweets-pr/tweets" element={<TweetPage />} />
         <Route path="*" element={<RestrictedRoute redirectTo="/" />} />
       </Routes>
     </>
